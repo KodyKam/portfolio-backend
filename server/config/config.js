8de485d5362cdd36dd1941339e7cc8654965c295
kodykam <bigachiever@icloud.com>
2025-07-20 22:32:09 -0400

    Use env var for MongoDB connection string

--- server/config/config.js
+++ server/config/config.js
@@ -4,8 +4,8 @@ const config = {
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://bigachiever:inCanada31@cluster0.fk33n7t.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0"
+    process.env.MONGODB_URI /* || */
+    // "mongodb+srv://bigachiever:inCanada31@cluster0.fk33n7t.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0"
 };
 
 export default config;
